3768826b3016ccfcce2f10f9c9a321b3093644f0
akashmondal2000 <mondalakash494@gmail.com>
2021-10-23 00:40:58 +0530

    Add some style

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,24 @@
-import style from "./styles.module.css"
+import style from "./styles.module.css";
 
 const Dashboard = () => {
-    return (
-        <div>
+  return (
+    <div>
+      <div className={style.header}>
         <h1 className={style.container}>Shopping Store</h1>
-            
-        </div>
-    );
+        <input className={style.searchBox} type="search" placeholder="Search" />
+        <h1 className={style.cartStyle}>cart</h1>
+      </div>
+
+      <div className = {style.itemStyle}>
+          <img className={style.imageStyle} src="https://5.imimg.com/data5/SELLER/Default/2021/4/OK/AE/HI/14160492/better-orenge-grafted-plant-500x500.jpg" 
+          alt="image not found" />
+
+          <p className ={style.descriptionStyle}>Description</p>
+          <p>$40</p>
+          <button type = "button">add</button>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
